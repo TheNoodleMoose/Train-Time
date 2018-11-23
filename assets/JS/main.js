559df52e0051316daa0f4939779328f0cb3c58ac
TheNoodleMoose <cjh605@gmail.com>
2018-11-22 22:17:47 -0500

    Got logic of the train scheduling to work, will attempt to update text every minute

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,4 +1,4 @@
-
+console.log("This refreshed")
 var config = {
     apiKey: "AIzaSyAglU4lTrdnh9JIf6qPDiD9Gx_UkPZjxTs",
     authDomain: "train-time-80752.firebaseapp.com",
@@ -7,6 +7,7 @@ var config = {
     storageBucket: "train-time-80752.appspot.com",
     messagingSenderId: "1057720917666"
 };
+
 firebase.initializeApp(config);
 
 var database = firebase.database();
@@ -15,9 +16,59 @@ var trainName = "";
 var destination = "";
 var frequency = "";
 var nextArrival = "";
-var firstTrain = "";
+var firstTrainTime = "";
 var minutesAway = "";
 
-$("#submitTrain").on("click", function() {
-    event.preventDefault();
+$("#add-user").on("click", function(button) {
+    button.preventDefault();
+    
+    trainName = $("#train-name-input").val().trim();
+    destination = $("#destination-input").val().trim();
+    firstTrainTime = $("#first-train-input").val().trim();
+    frequency = $("#frequency-input").val().trim();
+    var timeNow = moment();
+    console.log(timeNow)
+    firstTimeConverted = moment(firstTrainTime, "HH:mm").subtract(1 ,"years");
+    console.log(firstTimeConverted)
+    var differenceTime = moment().diff(moment(firstTimeConverted), "minutes");
+    console.log("DIFFERENCE IN TIME: " + differenceTime)
+    var tRemainder = differenceTime % frequency;
+    console.log(tRemainder)
+    var minutesTilNextTrain = frequency - tRemainder;
+    console.log("MINUTES TILL NEXT TRAIN: " + minutesTilNextTrain)
+    var nextTrainTime = moment().add(minutesTilNextTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrainTime).format("hh:mm"));
+    var nextTrainFormatted = moment(nextTrainTime).format("hh:mm");
+    database.ref().push({
+        trainName: trainName,
+        destination: destination,
+        firstTrainTime: firstTrainTime,
+        frequency: frequency,
+        minutesTilNextTrain: minutesTilNextTrain,
+        nextTrainFormatted: nextTrainFormatted,
+    });
+});
+
+database.ref().on("child_added", function (snapshot) {
+    var sv = snapshot.val();
+
+    console.log(sv.trainName)
+    console.log(sv.destination)
+    console.log(sv.minutesTilNextTrain)
+    console.log(sv.frequency)
+    console.log(sv.nextTrainFormatted)
+
+    var newRow = $("<tr>").append(
+        $("<td>").text(sv.trainName),
+        $("<td>").text(sv.destination),
+        $("<td>").text(`${sv.frequency} minutes`),
+        $("<td>").text(sv.nextTrainFormatted),
+        $("<td>").text(`${sv.minutesTilNextTrain} Minutes away`)
+
+    )
+
+    $("#newTrainsHere").append(newRow)
+
+}, function(errorObject) {
+    console.log("Errors handled: " + errorObject)
 })
